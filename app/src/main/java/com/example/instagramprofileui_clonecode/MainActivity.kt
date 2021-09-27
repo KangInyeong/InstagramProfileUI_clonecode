94fe907f239053f9e22ce088413e4159b6843d53
KangInyeong <52899340+KangInyeong@users.noreply.github.com>
2021-09-27 21:54:02 +0900

    Second commit after completing Instagram profile UI roughly

--- app/src/main/java/com/example/instagramprofileui_clonecode/MainActivity.kt
+++ app/src/main/java/com/example/instagramprofileui_clonecode/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.instagramprofileui_clonecode
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,28 +12,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.instagramprofileui_clonecode.ui.theme.InstagramProfileUI_clonecodeTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstagramProfileUI_clonecodeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            ProfileScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    InstagramProfileUI_clonecodeTheme {
-        Greeting("Android")
     }
 }
